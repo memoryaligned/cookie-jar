778df515fbc93cb8f4ce3b29004d9fc1b85ad6f4
Randall Adams <randall.adams@infinitive.com>
2024-01-30 09:08:45 -0500

    fix: broken unit test

--- fastapi/{{cookiecutter.repo_name}}/test/test_main.py
+++ fastapi/{{cookiecutter.repo_name}}/test/test_main.py
@@ -1,7 +1,8 @@
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from {{cookiecutter.repo_name}}.app import init_app
 
 
 def test_init_app():
     app = init_app()
-    assert isinstance(app, dict)
+    assert isinstance(app, FastAPI)
