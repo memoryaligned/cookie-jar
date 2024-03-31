0dc0c78f4e0b75a4a2e2db90c29130598b291fa1
randall adams <randall.e.adams@gmail.com>
2024-03-31 19:52:30 -0400

    feat: windows support and improved makefile

---
+++ ds/{{cookiecutter.repo_name}}/make.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+pushd %~dp0
+
+REM Command file to provide compatability for Windows systems
+
+if "%1" == "setup" {
+   python3 -m venv venv
+   venv/bin/python3 -m pip install -U pip setuptools wheel
+   venv/bin/python3 -m pip install -r requirements.txt
+   goto end
+}
+
+if "%1" == "clean" {
+   goto end
+}
+
+if "%1" == "lint" {
+   venv/bin/python3 -m flake8 scripts
+}
+
+if "%1" == "help" {
+   ECHO "Help..."
+}
+
+:end
+popd
