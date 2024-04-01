be2d0d2cebd5e084edff456b22b6e8e0817deb5d
Randall Adams <ml.at.scale.randy@gmail.com>
2024-04-01 13:17:29 -0400

    fix: corrected make.bat for Windows

--- ds/{{cookiecutter.repo_name}}/make.bat
+++ ds/{{cookiecutter.repo_name}}/make.bat
@@ -4,24 +4,25 @@ pushd %~dp0
 
 REM Command file to provide compatability for Windows systems
 
-if "%1" == "setup" {
+if "%1" == "setup" (
    python3 -m venv venv
    venv/bin/python3 -m pip install -U pip setuptools wheel
    venv/bin/python3 -m pip install -r requirements.txt
    goto end
-}
+)
 
-if "%1" == "clean" {
+if "%1" == "clean" (
    goto end
-}
+)
 
-if "%1" == "lint" {
+if "%1" == "lint" (
    venv/bin/python3 -m flake8 scripts
-}
+)
 
-if "%1" == "help" {
+if "%1" == "help" (
+:help
    ECHO "Help..."
-}
+)
 
 :end
 popd
