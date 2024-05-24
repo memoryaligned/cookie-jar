d3cb6aefe33f5059dd1afcb05cdb20c45115f9a8
Randall Adams <ml.at.scale.randy@gmail.com>
2024-05-24 12:11:44 -0400

    feat: implemented clean and dist

--- ds/{{cookiecutter.repo_name}}/make.bat
+++ ds/{{cookiecutter.repo_name}}/make.bat
@@ -12,6 +12,17 @@ if "%1" == "setup" (
 )
 
 if "%1" == "clean" (
+   rmdir /s /q dist
+   rmdir /s /q build
+   cd docs\01_statistical_analysis_plan
+   rmdir /s /q _build
+   cd ..\..\
+   cd docs\02_initial_data_analysis_report
+   rmdir /s /q _build
+   cd ..\..\
+   cd docs\03_exploratory_data_analysis_report
+   rmdir /s /q _build
+   cd ..\..\
    goto end
 )
 
@@ -20,20 +31,34 @@ if "%1" == "lint" (
    goto end
 )
 
-if %! == "sync_data_to_s3" (
-  echo not implemented
-  goto end
+if "%1" == "sync_data_to_s3" (
+   echo not implemented
+   goto end
 )
 
-if %! == "serve_doc" (
-  (cd docs\_build\html; ..\..\..\venv\Scripts\python -m http.server -b localhost)
-  goto end
+if "%1" == "serve_doc" (
+   (cd docs\_build\html; ..\..\..\venv\Scripts\python -m http.server -b localhost)
+   goto end
 )
 
-if %! == "dist" (
-  mkdir dist
-  cd build && zip ..\dist\docs.zip docs
-  goto end
+if "%1" == "dist" (
+   mkdir dist
+   mkdir build
+   cd docs\01_statistical_analysis_plan
+   set SPHINXBUILD=..\..\venv\Scripts\sphinx-build
+   make.bat html
+   cd _build
+   zip ..\..\..\dist\01_statistical_analysis_documentation.zip html
+   cd ..\..\..\
+   cd docs\02_initial_data_analysis_report
+   make.bat html
+   zip ..\..\..\dist\02_initial_data_analysis_report.zip html
+   cd ..\..\..\
+   cd docs\03_exploratory_data_analysis_report
+   make.bat html
+   zip ..\..\..\dist\03_exploratory_data_analysis_report.zip html
+   cd ..\..\..\
+   goto end
 )
 
 if "%1" == "help" (
